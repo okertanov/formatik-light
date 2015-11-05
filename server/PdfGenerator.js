6b24ec1701e336a97dffdec2c7bf68f5448244b3
okertanov <okertanov@gmail.com>
2015-11-05 11:38:25 +0200

    PdfGenerator & TemplateEngine module stubs

---
+++ server/PdfGenerator.js
@@ -0,0 +1,26 @@
+//
+// PdfGenerator.js
+//
+
+(function() {
+
+    "use strict";
+
+    //
+    // PdfGenerator
+    //
+    var PdfGenerator = function() {
+        return {
+            Initialize: function() {
+            },
+            Generate: function(name, options) {
+            }
+        };
+    };
+
+    //
+    // Module Exports
+    //
+    module.exports = PdfGenerator;
+
+})();
---
+++ server/TemplateEngine.js
@@ -0,0 +1,30 @@
+//
+// TemplateEngine.js
+//
+
+(function() {
+
+    "use strict";
+
+    //
+    // TemplateEngine
+    //
+    var TemplateEngine = function() {
+        return {
+            Initialize: function() {
+            },
+            Load: function(name) {
+            },
+            Save: function(name, template) {
+            },
+            Process: function(template, payload) {
+            }
+        };
+    };
+
+    //
+    // Module Exports
+    //
+    module.exports = TemplateEngine;
+
+})();
